cbbba8862793b0daf062357c901fe7e807c0b8a1
Kevin Ta <kvnta@yahoo.com>
2022-07-11 14:41:09 -0600

    Test: ship- exists, name, length and test renamed origin

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,18 @@
 require './lib/ship'
+
+RSpec.describe Ship do
+
+  cruiser = Ship.new("Cruiser", 3)
+
+  it 'exists' do
+    expect(ruiser.class).to be_a(Ship)
+  end
+
+  it 'has a name' do
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it 'has a length' do
+    expect(cruiser.length).to eq(3)
+  end  
+end
