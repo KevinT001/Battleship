ed75be944595a5453d4833bbdc522dc2f348df69
Kevin Ta <kvnta@yahoo.com>
2022-07-11 18:13:35 -0600

    Test: Cell - exists, coordinate, .ship, empty?

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,2 +1,25 @@
 require './lib/ship'
 require './lib/cell'
+
+RSpec.describe Cell do
+  before :each do
+    @cell = Cell.new("B4")
+  end
+
+  it 'exists' do
+    expect(@cell).to be_a(Cell)
+  end
+
+  it 'has coordinates' do
+    expect(@cell.coordinate).to eq("B4")
+  end
+
+  it 'has a cell with a ship?' do
+    expect(@cell.ship).to eq(nil)
+  end
+
+  it 'can tell if cell is empty?' do
+    expect(cell.empty?).to eq(true)
+  end
+
+end
