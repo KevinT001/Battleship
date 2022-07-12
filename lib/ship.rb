ee965d45e50351d295b261940f7f4a5a0c561dfa
Kyongnam Kang <kangkna@gmail.com>
2022-07-12 13:28:17 -0400

    Working on ship class and test

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,11 +1,14 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health, :hit
     def initialize(name, length)
       @name = name
       @length = length
+      @health = length
+
     end
 
-
+    def hit
+      @health -= 1
 
 
 
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Ship do
   cruiser = Ship.new("Cruiser", 3)
 
   it 'exists' do
-    expect(ruiser.class).to be_a(Ship)
+    expect(cruiser).to be_a(Ship)
   end
 
   it 'has a name' do
@@ -14,5 +14,17 @@ RSpec.describe Ship do
 
   it 'has a length' do
     expect(cruiser.length).to eq(3)
-  end  
+  end
+
+  it 'has health' do
+    expect(cruiser.health).to eq(3)
+  end
+
+  it 'health can get damaged with a hit' do
+    cruiser.hit
+    expect(cruiser.health).to eq(2)
+    cruiser.hit
+    expect(cruiser.health).to eq(1)
+  end
+
 end
