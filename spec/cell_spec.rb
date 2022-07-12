ddd0f7d2b8f25c1e93ba83a2f7aa6d5d5b38444d
Kevin Ta <kvnta@yahoo.com>
2022-07-12 00:25:17 -0600

    Test: Cell - render tests: for blank cell,ship present, fire miss, fire hit, fire sink. Iteration 1 tests complete

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -19,24 +19,68 @@ RSpec.describe Cell do
   end
 
   it 'can tell if cell is empty?' do
-    expect(cell.empty?).to eq(true)
+    expect(@cell.empty?).to eq(true)
   end
 
   it 'can place ship on cell' do
     cruiser = Ship.new("Cruiser", 3)
 
-    cell.place_ship(cruiser)
+    @cell.place_ship(cruiser)
     expect(cell.ship).to eq(cruiser)
     expect(cell.empty?). to eq(false)
   end
 
   it 'can see if cell is fired upon' do
-    expect(cell.fired_upon?).to eq(false)
+    expect(@cell.fired_upon?).to eq(false)
   end
 
   it 'can fire upon a cell' do
-    cell.fire_upon
-    expect(cell.ship.health).to eq(2)
-    expect(cell.fired_upon?).to eq(true)
+    @cell.fire_upon
+    expect(@cell.ship.health).to eq(2)
+    expect(@cell.fired_upon?).to eq(true)
+  end
+
+  it 'can render a (.)/ (M)' do
+    @cell_1 = Cell.new("B4")
+    expect(@cell_1.render).to eq(".") #renders a "." when a blank unfired upon cell is present
+
+    @cell_1.fire_upon
+    expect(@cell_1.render).to eq("M") # renders a "M" when fired upon cell is counted as a miss.
+  end
+
+  it 'cant test another cell' do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(cruiser)
+
+    expect(@cell_2.render).to eq(".")
+  end
+
+  it 'cell can render to show a ship placed' do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(cruiser)
+
+    expect(@cell_2.render(true)).to eq("S")
+  end
+
+  it 'cell can show H for hit when fired upon' do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(cruiser)
+    @cell_2.fire_upon
+    expect(@cell_2.render).to eq("H")
+    expect(@cruiser.sunk?).to eq(false)
+  end
+
+  it 'cells can show sunk ship with X' do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(cruiser)
+
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cruiser.sunk?).to eq(true)
+    expect(@cell_2.render).to eq("X")
   end 
 end
