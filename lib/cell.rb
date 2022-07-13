4b3c12fa9e96f9ea3ebac5a943e03f945d20b1eb
Kyongnam Kang <kangkna@gmail.com>
2022-07-12 23:43:12 -0400

    Add class cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,24 @@
 class Cell
+  attr_reader :coordinate,
+              :ship
+    def initialize(coordinate)
+      @coordinate = coordinate
+      @ship = nil
+
+    end
+
+    def empty?
+      if @ship == nil
+        return true
+      else
+        return false
+      end
+    end
+
+    def place_ship(boat)
+      @ship = boat
+
+    end
+
 
 end
