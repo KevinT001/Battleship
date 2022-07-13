c200893ecb2a6f7eb68258bf78c6c3c7548e5335
Kyongnam Kang <kangkna@gmail.com>
2022-07-12 23:01:50 -0400

    Add tests for cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,2 +1,29 @@
 require './lib/ship'
 require './lib/cell'
+
+RSpec.describe Cell do
+  let(cell) {Cell.new("B4")}
+
+  it 'exists' do
+    expect(cell).to be_a Cell
+  end
+
+  it 'has coordinate' do
+    expect(cell.coordinate).to eq("B4")
+  end
+
+  it 'does not contain ship' do
+    expect(cell.ship).to eq(nil)
+    expect(cell.empty?).to be true
+  end
+
+  it 'can contain ship' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+    expect(cell.ship).to eq(cruiser)
+    expect(cell.empty).to be false
+
+  end
+
+end
