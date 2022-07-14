28a065da2305ad2f6cc81af661685b40655901f7
Kyongnam Kang <kangkna@gmail.com>
2022-07-14 18:03:57 -0400

    Define render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,36 +5,39 @@ class Cell
   attr_reader :coordinate,
               :ship
 
-    def initialize(coordinate)
-      @coordinate = coordinate
-      @ship = nil
+  def initialize(coordinate)
+    @coordinate = coordinate.to_s
+    @ship = nil
+    @fired_upon = false
 
-    end
+  end
 
-    def empty?
-      if @ship == nil
-        return true
-      else
-        return false
-      end
-    end
+  def empty?
+    @ship == nil
+  end
 
-    def place_ship(boat)
+  def place_ship(boat)
       @ship = boat
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if !empty?
+      @ship.hit unless @fired_upon == true
     end
+    @fired_upon = true
+  end
 
-    def fired_upon?
-      if @ship.health == @ship.length
-        return false
-      else
-        return true
-      end
+  def render
+    return "." unless fired_upon?
+    return "M" if empty?
+    if @ship.sunk?
+    "X"
+    else
+    "H"
     end
-
-    def fire_upon
-      @ship.hit
-
-
-    end
-
+  end
 end
