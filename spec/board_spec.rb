3a073cb8d9ee8ebc794e91f76df14616068e96cf
Kevin Ta <kvnta@yahoo.com>
2022-07-15 00:05:43 -0600

    Test: diagonal coordinate check and valid coordinate for placement check

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -36,5 +36,14 @@ require './lib/board'
         expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq false
          expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq false
     end 
+    xit 'can check that coordinates are not diagonal' do 
+        expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq false 
+        expect(board.valid_placement?(submarine,["C2", "D3"])).to eq false
+    end 
+
+    xit 'can do a final check of valid coordinates for placement' do 
+        expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq true 
+        expect(board.valid_placement?(cruiser, ["B1", "C2","D1"])).to eq true 
+    end 
 end
 
