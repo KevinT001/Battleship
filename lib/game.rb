c92b39ec5d71661be68a575694ed0906a7f1c510
Kevin Ta <kvnta@yahoo.com>
2022-07-19 14:57:01 -0600

    Create game class and begin intialize
    Co-authored-by: Kyongnam <kangkna@gmail.com>

---
+++ lib/game.rb
@@ -0,0 +1,48 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game_messages'
+class Game
+    attr_reader :player_board,
+                :cpu_board,
+                :player_ships, 
+                :cpu_cruisers, 
+                :cpu_sub, 
+                :cpu_ships, 
+                :message
+
+    def initialize
+        @player_board = Board.new 
+        @cpu_board = Board.new 
+        @player
+    end
+
+
+
+
+
+
+
+
+
+    # create computer board
+    #create player board
+    # build game menu - welcome message, user input if they want to play or quit. (what if they quit, what happens?)
+    # if play then computer must place ships
+    #user prompted to place cruiser then place sub 
+    #display message of ships have been placed. 
+    #Player prompted to enter coordinates to shoot 
+    #player shoots and renders board(CPU?)(check if game is over?/all ships sunk)
+    # computer shoots. checks if game over? render player board
+    #loop above steps until cpu OR  player ships = sunk
+    #display message of winner. 
+    #loop back to game menu 
+
+    #message class? Need player info and cpu. 
+
+    def 
+        start
+    end
+end
+
+# methods 
