bc598c9c3d6e0341eec7153aea5474ef9df57f92
Kevin Ta <kvnta@yahoo.com>
2022-07-19 17:08:05 -0600

    Pseudo code some messages for game
    Co-authored-by: Kyongnam <kangkna@gmail.com>

--- lib/game_messages.rb
+++ lib/game_messages.rb
@@ -1,3 +1,4 @@
+require './lib/'
 class Message
 
     def welcome 
@@ -21,6 +22,43 @@ class Message
         puts "You now need to lay out your two ships."
         puts "The Cruiser is three units long and the Submarine is two unites long."
     end
+
+    def player_sub_placement
+        puts "Enter the squares for the Submarine (2 spaces)."
+    end
+
+    def player_cruiser_placement 
+        puts "Enter the squares for the Cruiser (3 spaces)."
+    end
+
+    def invalid_coordinates
+        puts "Invalid coordinates. Please enter again."
+    end
+
+    def player_shot
+        if #cpu ship is hit
+            puts "Hit"
+        else
+            puts "Your shot missed"
+        end
+    end
+
+    def computer_shot
+        if #cpu hits player ship
+            puts "Computer shot hit"
+        else
+            puts "Computer missed"
+        end
+    end
+
+    def winner
+        if #player wins
+            puts "You won!"
+        else #computer wins (all player ships sunk)
+            "You lose!"
+        end
+    end
+
 
 
 end
