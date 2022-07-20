a7aace079f808802362531a5d926b74e2995d175
Kyongnam Kang <kangkna@gmail.com>
2022-07-20 10:05:29 -0400

    Add fired upon method

--- lib/game.rb
+++ lib/game.rb
@@ -5,15 +5,15 @@ require './lib/message'
 class Game
     attr_reader :player_board,
                 :cpu_board,
-                :player_ships, 
-                :cpu_cruisers, 
-                :cpu_sub, 
-                :cpu_ships, 
+                :player_ships,
+                :cpu_cruisers,
+                :cpu_sub,
+                :cpu_ships,
                 :message
 
     def initialize
-        @player_board = Board.new 
-        @cpu_board = Board.new 
+        @player_board = Board.new
+        @cpu_board = Board.new
         @player_sub = Ship.new("Submarine", 2)
         @player_cruiser = Ship.new("Cruiser", 3)
         @cpu_cruiser = Ship.new("Cruiser", 3)
@@ -23,18 +23,18 @@ class Game
         @player_ships = 2
     end
 
-    def run 
+    def run
         puts message.welcome
-        user_input = gets.chomp.downcase 
+        user_input = gets.chomp.downcase
          if user_input == "q"
-            exit 
+            exit
          end
-        
+
         coordinates = random_coordinates(@cpu_cruiser, @cpu_board)
         coordinates2 = random_coordinates(@cpu_sub, @cpu_board)
         @cpu_board.place_ship(@cpu_cruiser,coordinates)
         @cpu_board.place_ship(@cpu_sub,coordinates2)
-        
+
         # puts message.computer_board ( why does this not place sub ship?)
         puts @cpu_board.render(false)
         puts message.player_board
@@ -42,12 +42,12 @@ class Game
         puts message.cpu_ship_placment
         puts message.player_ship_placement
         puts message.player_cruiser_placement
-    
-        
+
+
         is_valid_coordinates = false
-         while is_valid_coordinates == false do 
+         while is_valid_coordinates == false do
             user_coords = gets.chomp.upcase.split(" ")
-            if @player_board.valid_placement?(@player_cruiser,user_coords) == true 
+            if @player_board.valid_placement?(@player_cruiser,user_coords) == true
                 @player_board.place_ship(@player_cruiser, user_coords)
                 puts message.computer_board
                 puts @cpu_board.render(false)
@@ -55,10 +55,10 @@ class Game
                 puts @player_board.render(true)
                 is_valid_coordinates == true
                 break
-            else puts message.invalid_coordinates 
-            end 
+            else puts message.invalid_coordinates
+            end
         end
-        
+
         puts message.computer_board
         puts @cpu_board.render(false)
          puts message.player_board
@@ -66,20 +66,20 @@ class Game
          puts message.player_sub_placement
 
          is_valid_coordinates = false
-         while is_valid_coordinates == false do 
+         while is_valid_coordinates == false do
             user_coords = gets.chomp.upcase.split(" ")
-            if @player_board.valid_placement?(@player_sub,user_coords) == true 
+            if @player_board.valid_placement?(@player_sub,user_coords) == true
                 @player_board.place_ship(@player_sub, user_coords)
-                
+
                 puts message.computer_board
                 puts @cpu_board.render(false)
                 puts message.player_board
                 puts@player_board.render(true)
-                is_valid_coordinates == true 
+                is_valid_coordinates == true
                 break
-            else puts message.invalid_coordinates 
+            else puts message.invalid_coordinates
             end
-        end 
+        end
 
           #player turn to shoot
         puts message.player_shoots
@@ -93,7 +93,7 @@ class Game
             if @cpu_board.cells["#{player_shot}"].render == "X"
             puts "You sunk a #{@cpu_board.cells["#{player_shot}"].ship.name}!"
             @cpu_ships -= 1
-                if @cpu_ships == 0 
+                if @cpu_ships == 0
                     puts message.winner
                     run
                 end
@@ -107,34 +107,59 @@ class Game
         puts message.invalid_coordinates
         player_turn
         end
-    
+
 
         puts message.computer_board
         puts @cpu_board.render(false)
         puts message.player_board
         puts@player_board.render(true)
-        
-    end 
-            
-        
 
-        
+        computer_shot = @player_board.cells.to_a.sample(1).flatten[0]
+        duplicate_shot = @player_board.cells["#{computer_shot}"].render != "."
+        if !duplicate_shot
+        @player_board.cells["#{computer_shot}"].fire_upon
+        if @player_board.cells["#{computer_shot}"].render == "X"
+            puts "The computer sunk your #{@player_board.cells["#{computer_shot}"].ship.name}!"
+            @player_ships -= 1
+                if @player_ships == 0
+                    puts message.loser
+                    run
+                end
+        elsif @player_board.cells["#{computer_shot}"].render == "H"
+            puts "The computer's shot on #{computer_shot} was a hit."
+        elsif @player_board.cells["#{computer_shot}"].render == "M"
+            puts "The computer's shot on #{computer_shot} was a miss."
+        end
+        else
+        computer_turn
+        end
+
+        puts message.computer_board
+        puts @cpu_board.render(false)
+        puts message.player_board
+        puts@player_board.render(true)
+
+    end
 
 
-    
+
+
+
+
+
 
     def random_coordinates(boat, board)
-        valid_coordinates = [] 
-        until board.valid_placement?(boat, valid_coordinates) do 
+        valid_coordinates = []
+        until board.valid_placement?(boat, valid_coordinates) do
             coordinates = board.cells.keys
             start = rand(coordinates.size)
             length = boat.length
-            valid_coordinates = coordinates.slice(start, length) 
-            
+            valid_coordinates = coordinates.slice(start, length)
+
         end
         valid_coordinates
     end
-end 
+end
 
 
 
@@ -149,19 +174,19 @@ end
     #create player board
     # build game menu - welcome message, user input if they want to play or quit. (what if they quit, what happens?)
     # if play then computer must place ships
-    #user prompted to place cruiser then place sub 
-    #display message of ships have been placed. 
+    #user prompted to place cruiser then place sub
+    #display message of ships have been placed.
 
-    #Player prompted to enter coordinates to shoot 
+    #Player prompted to enter coordinates to shoot
     #player shoots and renders board(CPU?)(check if game is over?/all ships sunk)
     # computer shoots. checks if game over? render player board
     #loop above steps until cpu OR  player ships = sunk
-    #display message of winner. 
-    #loop back to game menu 
+    #display message of winner.
+    #loop back to game menu
 
-    #message class? Need player info and cpu. 
-
-  
+    #message class? Need player info and cpu.
 
 
-# methods 
+
+
+# methods
