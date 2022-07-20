454eed871e3414b1ad427a3f64af59503c4d7b36
Kyongnam Kang <kangkna@gmail.com>
2022-07-20 15:16:41 -0400

    Delete @ symbold for computer and player board

--- lib/game.rb
+++ lib/game.rb
@@ -32,13 +32,13 @@ class Game
 
         coordinates = random_coordinates(@cpu_cruiser, @cpu_board)
         coordinates2 = random_coordinates(@cpu_sub, @cpu_board)
-        @cpu_board.place_ship(@cpu_cruiser,coordinates)
-        @cpu_board.place_ship(@cpu_sub,coordinates2)
+        cpu_board.place_ship(@cpu_cruiser,coordinates)
+        cpu_board.place_ship(@cpu_sub,coordinates2)
 
         # puts message.computer_board ( why does this not place sub ship?)
-        puts @cpu_board.render(false)
+        puts cpu_board.render(false)
         puts message.player_board
-        puts @player_board.render(false)
+        puts player_board.render(false)
         puts message.cpu_ship_placment
         puts message.player_ship_placement
         puts message.player_cruiser_placement
@@ -47,12 +47,12 @@ class Game
         is_valid_coordinates = false
          while is_valid_coordinates == false do
             user_coords = gets.chomp.upcase.split(" ")
-            if @player_board.valid_placement?(@player_cruiser,user_coords) == true
-                @player_board.place_ship(@player_cruiser, user_coords)
+            if player_board.valid_placement?(@player_cruiser,user_coords) == true
+                player_board.place_ship(@player_cruiser, user_coords)
                 puts message.computer_board
-                puts @cpu_board.render(false)
+                puts cpu_board.render(false)
                 puts message.player_board
-                puts @player_board.render(true)
+                puts player_board.render(true)
                 is_valid_coordinates == true
                 break
             else puts message.invalid_coordinates
@@ -60,21 +60,21 @@ class Game
         end
 
         puts message.computer_board
-        puts @cpu_board.render(false)
+        puts cpu_board.render(false)
         puts message.player_board
-        puts @player_board.render(true)
+        puts player_board.render(true)
         puts message.player_sub_placement
 
          is_valid_coordinates = false
          while is_valid_coordinates == false do
             user_coords = gets.chomp.upcase.split(" ")
-            if @player_board.valid_placement?(@player_sub,user_coords) == true
-                @player_board.place_ship(@player_sub, user_coords)
+            if player_board.valid_placement?(@player_sub,user_coords) == true
+                player_board.place_ship(@player_sub, user_coords)
 
                 puts message.computer_board
-                puts @cpu_board.render(false)
+                puts cpu_board.render(false)
                 puts message.player_board
-                puts@player_board.render(true)
+                puts player_board.render(true)
                 is_valid_coordinates == true
                 break
             else puts message.invalid_coordinates
@@ -91,28 +91,28 @@ class Game
         until @cpu_ships == 0 || @player_ships == 0
         puts message.player_shoots
         player_shot = gets.chomp.upcase.strip
-        if @cpu_board.valid_coordinate?(player_shot)
-        if @cpu_board.cells["#{player_shot}"].fired_upon?
+        if cpu_board.valid_coordinate?(player_shot)
+        if cpu_board.cells["#{player_shot}"].fired_upon?
             player_shot
             puts "You cannot fire upon the same cell twice."
 
 
         else
-            @cpu_board.cells["#{player_shot}"].fire_upon
-            if @cpu_board.cells["#{player_shot}"].render == "X"
+            cpu_board.cells["#{player_shot}"].fire_upon
+            if cpu_board.cells["#{player_shot}"].render == "X"
             puts "You sunk a #{@cpu_board.cells["#{player_shot}"].ship.name}!"
-            @cpu_ships -= 1
-                if  @cpu_ships == 0
+            cpu_ships -= 1
+                if  cpu_ships == 0
                     puts message.winner
-                    @cpu_board = Board.new
-                    @player_board = Board.new
-                    @cpu_ships = @player_ships = 2
+                    cpu_board = Board.new
+                    player_board = Board.new
+                    cpu_ships = player_ships = 2
                     run
 
                 end
-            elsif @cpu_board.cells["#{player_shot}"].render == "H"
+            elsif cpu_board.cells["#{player_shot}"].render == "H"
             puts "Your shot on #{player_shot} was a hit!"
-            elsif @cpu_board.cells["#{player_shot}"].render == "M"
+            elsif cpu_board.cells["#{player_shot}"].render == "M"
             puts "Your shot on #{player_shot} was a miss."
             end
         end
@@ -123,28 +123,28 @@ class Game
 
 
         puts message.computer_board
-        puts @cpu_board.render(false)
+        puts cpu_board.render(false)
 
         puts message.player_board
-        puts @player_board.render(true)
+        puts player_board.render(true)
 
-        computer_shot = @player_board.cells.to_a.sample(1).flatten[0]
-        duplicate_shot = @player_board.cells["#{computer_shot}"].render != "."
-        @player_board.cells["#{computer_shot}"].fire_upon
-        if @player_board.cells["#{computer_shot}"].render == "X"
+        computer_shot = player_board.cells.to_a.sample(1).flatten[0]
+        duplicate_shot = player_board.cells["#{computer_shot}"].render != "."
+        player_board.cells["#{computer_shot}"].fire_upon
+        if player_board.cells["#{computer_shot}"].render == "X"
             puts "The computer sunk your #{@player_board.cells["#{computer_shot}"].ship.name}!"
-            @player_ships -= 1
-                if @player_ships == 0
+            player_ships -= 1
+                if player_ships == 0
                     puts message.loser
-                    @cpu_board = Board.new
-                    @player_board = Board.new
-                    @cpu_ships = @player_ships = 2
+                    cpu_board = Board.new
+                    player_board = Board.new
+                    cpu_ships = player_ships = 2
                     run
 
                 end
-        elsif @player_board.cells["#{computer_shot}"].render == "H"
+        elsif player_board.cells["#{computer_shot}"].render == "H"
             puts "The computer's shot on #{computer_shot} was a hit."
-        elsif @player_board.cells["#{computer_shot}"].render == "M"
+        elsif player_board.cells["#{computer_shot}"].render == "M"
             puts "The computer's shot on #{computer_shot} was a miss."
         end
 
@@ -152,9 +152,9 @@ class Game
 
 
         puts message.computer_board
-        puts @cpu_board.render(false)
+        puts cpu_board.render(false)
         puts message.player_board
-        puts @player_board.render(true)
+        puts player_board.render(true)
       end
     end
 
